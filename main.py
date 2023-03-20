0ba55210b6a1cb45b165a7b4edba9db04f946302
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2023-03-20 21:54:38 +0000

    more practice with objects

--- main.py
+++ main.py
@@ -13,8 +13,19 @@ class House:
 
   def area(self, width, height):
     return width * height
-  
+
+  def printOut():
+    print(f'this house has {windows} windows, the color is {color} and {floors} floors')
+
+    
 redHouse = House(3, 15, 20, "red", 2, True)
 print(redHouse.area(34, 56))
+print(redHouse.printOut())
+
 blueHouse = House(5, 40, 65, "blue", 3, True)
 print(blueHouse.area(54,25))
+print(blueHouse.printOut())
+
+orangeHouse = House(5, 15, 25, "orange", 1, False)
+print(orangeHouse.area(51, 47))
+print(orangeHouse.printOut())
